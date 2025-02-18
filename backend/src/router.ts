9876a461cf18701621762d167dad5593178224c7
Kaio Lima <kaiolima.asus@gmail.com>
2025-02-18 15:09:21 -0300

    alterando backend

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -5,7 +5,6 @@ import UserController from "./controllers/user/UserController";
 
 import uploadConfig from './config/multer'
 
-import RecaptchaController from "./controllers/recaptcha/recaptchaController"
 
 const upload = multer(uploadConfig.upload("./tmp"));
 
@@ -15,7 +14,7 @@ const router = Router();
 router.post("/send", upload.single("file"), UserController.handle);
 
 
-router.post("/verify-recaptcha", RecaptchaController.handle)
+
 
 
 export default router;
